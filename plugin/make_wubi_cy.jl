a4b61d4a27e5cff9c94a8ab1a381126e92203caa
Kaiyin Zhong <k.zhong@gemvision.io>
2018-09-28 18:48:49 +0200

    clean up the wubi data

--- plugin/make_wubi_cy.jl
+++ plugin/make_wubi_cy.jl
@@ -1,13 +1,14 @@
 using DelimitedFiles
-wb_filepath = "wubi98.cy"
-wubi = readdlm(wb_filepath)
+wb_filepath = "wubi86.txt"
+wubi = readdlm(open(wb_filepath))
 nrows, ncols = size(wubi)
-for i = 1:nrows
-    tmp = wubi[i, 1]
-    wubi[i,1] = wubi[i,2]
-    wubi[i,2] = tmp
-end
+# for i = 1:nrows
+#     tmp = wubi[i, 1]
+#     wubi[i,1] = wubi[i,2]
+#     wubi[i,2] = tmp
+# end
 writedlm(wb_filepath, wubi, " ")
 
 codes = convert(Array{String}, wubi[:, 1])
 maximum( map(length, codes) )
+sort(unique(join(codes)))
